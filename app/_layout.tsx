fa7dfc824eb0993890bb58561f18c0f2e6c6a434
youuiiiii <richie.christian@student.umn.ac.id>
2026-05-17 22:23:46 +0700

    hapus yg gapenting

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+
 import { AuthProvider } from "../src/contexts/AuthContext";
 
 export default function RootLayout() {
